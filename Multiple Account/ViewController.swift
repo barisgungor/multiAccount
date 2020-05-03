acb7c4c38ec694ce5fef757f01684e8873362894
Barış Güngör <barisgungor@Bars-MacBook-Pro.local>
2020-05-03 12:30:33 +0300

    first commit

---
+++ Multiple Account/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Multiple Account
+//
+//  Created by Barış Güngör on 3.05.2020.
+//  Copyright © 2020 Barış Güngör. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
